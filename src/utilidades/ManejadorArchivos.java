661707c3cb52b282fabe292d5623ee953cde3a2d
Vinni <vinni9@gmail.com>
2021-02-25 19:41:47 -0500

    Clase Manejador de archivos.

---
+++ src/utilidades/ManejadorArchivos.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utilidades;
+
+import logica.Prestamo;
+
+/**
+ *
+ * @author Vinni
+ */
+public class ManejadorArchivos {
+    
+    private static final String archivo = "Prestamsos.ubc";
+    //Fecha :  nombre / candidato / valor solicitado / tasa / cuota
+    public static boolean guardar(Prestamo p){
+        
+        return false;
+    }
+    
+}
